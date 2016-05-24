4aee42f2229d2642168712ff0bed95ce1395ba0f
me-alina <ignea.alina@gmail.com>
2016-05-24 16:36:33 +0300

    Login, Register, Logout ++
    done with php , mysql and sqlite

---
+++ app/src/main/java/alinaignea/licenta/AppConfig.java
@@ -0,0 +1,12 @@
+package alinaignea.licenta;
+
+/**
+ * Created by Alina Ignea on 5/15/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://10.0.2.2/licenta/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://10.0.2.2/licenta/register.php";
+}
---
+++ app/src/main/java/alinaignea/licenta/menu_classes/EditProfile.java
@@ -0,0 +1,7 @@
+package alinaignea.licenta.menu_classes;
+
+/**
+ * Created by Alina Ignea on 5/19/2016.
+ */
+public class EditProfile {
+}
