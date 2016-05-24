41e79097a439118611275c853ac232d8c0fed44c
me-alina <ignea.alina@gmail.com>
2016-05-24 16:55:13 +0300

    php code for db access
    login , register - placed in localhost

---
+++ php/include/test.php
@@ -0,0 +1,5 @@
+<?php
+mysqli_connect('127.0.0.1', 'root', 'root','carsharing') or die('Could not connect the database : Username or password incorrect');
+
+echo 'Database Connected successfully';
+?>
