ffec64fc9217c4e713cc4e7eebfa3d76d3e0263d
me-alina <ignea.alina@gmail.com>
2016-06-04 23:40:58 +0300

    getting user from db + pic

--- app/src/main/java/alinaignea/licenta/menu_classes/ViewProfile.java
+++ app/src/main/java/alinaignea/licenta/menu_classes/ViewProfile.java
@@ -3,12 +3,17 @@ package alinaignea.licenta.menu_classes;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.os.AsyncTask;
@@ -26,7 +31,10 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -45,7 +53,7 @@ import alinaignea.licenta.helper.SessionManager;
  * Created by Alina Ignea on 5/19/2016.
  */
 public class ViewProfile extends Activity {
-
+    protected Bitmap bitmap;
     private Button btnEdit;
     private TextView txtName;
     private TextView txtEmail;
@@ -53,6 +61,9 @@ public class ViewProfile extends Activity {
     private TextView txtCity;
     private TextView txtAbout;
     private ProgressDialog pDialog;
+    private RatingBar ratingVal;
+    private ImageView picture;
+
     String myJSON;
     private SessionManager session;
     private SQLiteHandler db;
@@ -69,8 +80,9 @@ public class ViewProfile extends Activity {
         txtEmail = (TextView) findViewById(R.id.txtemail);
         txtPhone = (TextView) findViewById(R.id.txtphone);
         txtCity = (TextView) findViewById(R.id.txtcity);
-        txtAbout = (TextView) findViewById(R.id.txtabout);
-
+        txtAbout = (TextView) findViewById(R.id.aboutInfo);
+        ratingVal = (RatingBar) findViewById(R.id.ratingBar);
+        picture = (ImageView) findViewById(R.id.pic);
 
         // SQLite database handler
         db = new SQLiteHandler(getApplicationContext());
@@ -88,7 +100,7 @@ public class ViewProfile extends Activity {
         txtName.setText(name);
         txtEmail.setText(email);
 
-       if (!email.isEmpty())
+        if (!email.isEmpty())
             getUserData(email);
 
 
@@ -122,105 +134,144 @@ public class ViewProfile extends Activity {
 
     }
 
-    protected void showData(String result){
 
-        try {
-            JSONObject jObj = new JSONObject(result);
-            boolean error = jObj.getBoolean("error");
-
-            // Check for error node in json
-            if (!error) {
-                // user profile successfully fetched
-
-                // Now show profile
-                String uid = jObj.getString("uid");
-
-                JSONObject user = jObj.getJSONObject("user");
-
-                String created_at = user.getString("created_at");
-                String phone = user.getString("phone");
-                //   String city = user.getString("city");
-                //   String about = user.getString("about");
-                //   String rating_cnt = user.getString("rating_cnt");
-                //   String rating_val = user.getString("rating_val");
-
-
-                txtPhone.setText(phone);
-                //  txtCity.setText(city);
-                //  txtAbout.setText(about);
-
-
-            } else {
-                // Error in login. Get the error message
-                String errorMsg = jObj.getString("error_msg");
-                Toast.makeText(getApplicationContext(),
-                        errorMsg, Toast.LENGTH_LONG).show();
-            }
-        } catch (JSONException e) {
-            // JSON error
-            e.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
-    }
 
     private void getUserData(final String email) {
 
-            class GetDataJSON extends AsyncTask<String, Void, String>{
+        class GetDataJSON extends AsyncTask<String, Void, String>{
 
-                @Override
-                protected String doInBackground(String... params) {
+            @Override
+            protected String doInBackground(String... params) {
 
-                    HttpClient httpclient = new DefaultHttpClient();
-                    HttpPost httppost = new HttpPost(AppConfig.URL_PROFILE);
 
-                    // Depends on your web service
-                    httppost.setHeader("Content-type", "application/json");
-                    try {
-                        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
-                        nameValuePairs.add(new BasicNameValuePair("email", email));
-                        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                        } catch (UnsupportedEncodingException e)
-                        {
-                            e.printStackTrace();
-                        }
-                    InputStream inputStream = null;
-                    String result = null;
-                    try {
-                        HttpResponse response = httpclient.execute(httppost);
-                        HttpEntity entity = response.getEntity();
 
-                        inputStream = entity.getContent();
-                        // json is UTF-8 by default
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
-                        StringBuilder sb = new StringBuilder();
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost(AppConfig.URL_PROFILE);
 
-                        String line = null;
-                        while ((line = reader.readLine()) != null)
-                        {
-                            sb.append(line + "\n");
-                        }
-                        result = sb.toString();
-                    } catch (Exception e) {
-                        // Oops
-                    }
-                    finally {
-                        try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
-                    }
-                    return result;
+                // Depends on your web service
+                httppost.setHeader("Content-type", "application/x-www-form-urlencoded");
+                try {
+                    ArrayList<NameValuePair> nameValuePairs;
+                    nameValuePairs = new ArrayList<NameValuePair>();
+                    nameValuePairs.add(new BasicNameValuePair("email", email));
+                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                } catch (UnsupportedEncodingException e)
+                {
+                    e.printStackTrace();
                 }
+                InputStream inputStream = null;
+                String result = null;
+                try {
+                    HttpResponse response = httpclient.execute(httppost);
+                    HttpEntity entity = response.getEntity();
 
-                @Override
-                protected void onPostExecute(String result){
+                    inputStream = entity.getContent();
+                    // json is UTF-8 by default
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
 
-                    showData(result);
-
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                    Log.d("php", result);
+                } catch (Exception e) {
+                    // Oops
                 }
+                finally {
+                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+                }
+                return result;
             }
+
+            @Override
+            protected void onPostExecute(String result){
+
+                try {
+                    JSONObject jObj = new JSONObject(result);
+                    boolean error = jObj.getBoolean("error");
+
+                    // Check for error node in json
+                    if (!error) {
+                        // user profile successfully fetched
+
+                        // Now show profile
+                        String uid = jObj.getString("uid");
+
+                        JSONObject user = jObj.getJSONObject("user");
+
+                        String created_at = user.getString("created_at");
+                        String phone = user.getString("phone");
+                        String city = user.getString("city");
+                        String about = user.getString("about");
+                        String rating_cnt = user.getString("rating_cnt");
+                        String rating_val = user.getString("rating_val");
+                        String avatar = user.getString("avatar");
+                        float rating_v = Float.parseFloat(rating_val);
+
+                        txtPhone.setText(phone);
+                        txtCity.setText (city);
+                        txtAbout.setText(about);
+                        ratingVal.setStepSize((float)0.1);
+                        ratingVal.setRating(rating_v);
+
+                        new LoadImage().execute(avatar);
+
+                    }
+
+                    else {
+                        // Error in login. Get the error message
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }
         GetDataJSON g = new GetDataJSON();
         g.execute();
+    }
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(ViewProfile.this);
+            pDialog.setMessage("Loading Image ....");
+            pDialog.show();
+
+        }
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream)new URL(args[0]).getContent());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
         }
 
+        protected void onPostExecute(Bitmap image) {
+
+            if(image != null){
+                picture.setImageBitmap(image);
+                pDialog.dismiss();
+
+            }else{
+
+                pDialog.dismiss();
+                Toast.makeText(ViewProfile.this, "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();
+
+            }
+        }
     }
+}
 
 
